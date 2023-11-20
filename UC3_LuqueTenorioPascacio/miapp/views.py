11a25b6bfd22ea9765429d984750778e2a735733
Marjorie Luque <luquecardenasmarjorienikhol@gmail.com>
2023-11-20 10:26:40 -0500

    UC3_pARTE01_LUQUE

---
+++ UC3_LuqueTenorioPascacio/miapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+from sympy import primerange
+
+def home(request):
+    context = {
+        'title': 'UNTELS',
+        'content': 'Este es tu contenido personalizado.',
+    }
+    return render(request, 'miapp/home.html', context)
+
